25a396174a53ea2feb0ee2510bb9603bd6776c6a
HauNguyen1301 <haunh1301@gmail.com>
2024-12-31 14:24:03 +0700

    submit beta version
    submit beta version

--- Convert_Excel_to_SQlite.py
+++ Convert_Excel_to_SQlite.py
@@ -1,25 +1,16 @@
 import pandas as pd
 import sqlite3
 import os
+import gzip
 
-# Step 1: Load the Excel file into a DataFrame
-excel_file = r"F:\PBI Ban\Python Project\Crawl Drugbank\Drugbank_DB.xlsx"  # Replace with your file name
-sheet_name = "Sheet1"  # Replace with your sheet name if necessary
-df = pd.read_excel(excel_file, sheet_name=sheet_name)
-
-# Step 2: Connect to an SQLite database (or create one if it doesn't exist)
-
-# Step 2: Đặt đường dẫn đến thư mục lưu file SQLite
-output_dir = r"F:\PBI Ban\Python Project\Crawl Drugbank"  # Thư mục đích
-sqlite_db = os.path.join(output_dir, "drugDB.sqlite")  # Tên file SQLite
-conn = sqlite3.connect(sqlite_db)
-
-
-# Step 3: Write the DataFrame to an SQLite table
-table_name = "druginfo"  # Replace with your desired table name
-df.to_sql(table_name, conn, if_exists="replace", index=False)
-
-# Step 4: Close the database connection
+df = pd.read_csv('druginfomation.csv')
+df['soQuyetDinh'] = df['thongTinDangKyThuoc'].str.extract(r"'soQuyetDinh': '([^']+)'")
+df['hoatChatChinh'] = df['thongTinThuocCoBan'].str.extract(r"'hoatChatChinh': '([^']+)'")
+conn = sqlite3.connect('DrugDB.db')
+df.to_sql('drugInformation', conn, if_exists='replace', index=False)
 conn.close()
+with open('DrugDB.db', 'rb') as f_in, gzip.open('DrugDB.db.gz', 'wb') as f_out:
+    f_out.writelines(f_in)
 
-print(f"Data successfully written to {sqlite_db} in table '{table_name}'.")
+
+print("DataFrame successfully converted to SQLite database.")
